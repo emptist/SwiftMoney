fe4a32a6e936262cdbe30cb470cf1fe3c28849ad
jigme1968 <jigme1968@gmail.com>
2020-08-30 10:22:50 +0800

    Initial Commit

---
+++ SwiftMoney/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftMoney
+//
+//  Created by jk on 2020/8/30.
+//  Copyright © 2020 ssl. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
